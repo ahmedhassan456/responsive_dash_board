a41af58a473841393392fe2a11f7c1cc3cbed3d1
Ahmed Saqr <ahmedhassansaqr28@gmail.com>
2024-01-26 17:13:35 +0200

    create dashboard desktop layout

---
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DashboardTesktopLayout extends StatelessWidget {
+  const DashboardTesktopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(),
+    );
+  }
+}
